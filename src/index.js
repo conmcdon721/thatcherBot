003e77345a5e61f3bb691fdb9f3d14387eb79967
conmcdon721 <81780150+conmcdon721@users.noreply.github.com>
2021-09-08 22:13:58 -0400

    Feat: Add hours and mins, fix month discrepancy
    I started on this update by cleaning up the code I had already written.
    A lot of the conventions were from TechnoTim, whose tutorial I used to
    learn how to make a Twitch bot. I deleted the unnecessary app.js entry
    and consolidated code into index.js. After that, I refactored the token
    variables into a .env file.
    
    Most of what was changed for this update was in the time-logic.js file.
    Previously, I couldn't figure out how to account for discrepancies in
    each month's number of days (i.e. 28/30/31). My leap year script was
    working pretty well, but I wanted to be as specific as possible when
    it came to how many days had passed and how many months that
    consequently was.
    
    The solution, I found, was to create an array that contained the number
    of days that have passed since Maggie died for each month. So, in May,
    30 days have passed since she died. In June, 61 days have passed. In
    July, 91 days, so on and so forth. Using the number of days left over
    from calculating the number of years that have passed, I was able to
    iterate through the array and choose the appropriate number of months
    based on the arrays index. Then, I could use those numbers to calculate
    days, hours, and minutes.
    
    Finally, I threw all of that into an exported variable and put it into
    index.js. I used to have the function saved in a const that was
    exported, but this didn't update frequently enough. I'll be testing
    regularly to see if the numbers are still accurate in the am.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,30 @@
-require = require('esm')(module/*, options*/)
-module.exports = require('./app.js')
+const tmi = require('tmi.js');
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const calculator = require('./time_logic');
+
+const options = {
+	options: { debug: true, messagesLogLevel: "info" },
+	connection: {
+		reconnect: true,
+		secure: true
+	},
+	identity: {
+		username: process.env.BOT_USERNAME,
+		password: process.env.OAUTH_TOKEN
+	},
+	channels: [ process.env.CHANNEL_NAME ]
+}
+
+const client = new tmi.Client(options);
+
+client.connect().catch(console.error);
+
+client.on('message', (channel, userstate, message, self) => {
+	if(self) return;
+	if(message.toLowerCase() === '!isthatcherdead') {
+		client.say(channel, `@${userstate.username}, ${calculator.calculator()}`);
+	}
+});
--- src/time_logic.js
+++ src/time_logic.js
@@ -1,34 +1,54 @@
-let today = new Date();
-let maggieDeathDate = new Date('April 8, 2013 06:28:00')
-
-const leapYearFinder = () => {
-    let leapYears = [];
-    let todaysYear = today.getFullYear();
-
-    for (let i = 2013; i < todaysYear; i ++) {
-        let currentRemainder = i % 4;
-        if (currentRemainder === 0) {
-            leapYears.push(i);
-        }
-    };
-
-    let daysToSub = leapYears.length;
-    return daysToSub;
-}
-
-let leapDays = leapYearFinder();
 
 const calculator = () => {
-    let dayDiff = ((((today - maggieDeathDate) / 1000) / 60) / 60) / 24;
-    dayDiff -= leapDays;
+    let today = new Date();
+    let maggieDeathDate = new Date('April 8, 2013 06:28:00')
+    
+    const leapYearFinder = () => {
+        let leapYears = [];
+        let todaysYear = today.getFullYear();
+    
+        for (let i = 2013; i < todaysYear; i ++) {
+            let currentRemainder = i % 4;
+            if (currentRemainder === 0) {
+                leapYears.push(i);
+            };
+        };
+    
+        let leapYearDays = leapYears.length;
+        return leapYearDays;
+    }
+    
+    let leapDays = leapYearFinder();
 
-    let years = Math.round(dayDiff / 365)
-    let months = Math.round((dayDiff % 365) / 30)
-    let days = Math.round((dayDiff % 365) % 30)
+    /* ------------------------------------------------------------------------------------------------------------------------------------------------ */
 
-    return `Yes! Margaret Thatcher is indeed dead. She has been dead for ${years} years, ${months} month(s), and ${days} day(s).`
+    const monthArray = [30, 61, 91, 122, 153, 183, 214, 244, 275, 306, 334]
+
+    let milliSinceDeath = today - maggieDeathDate
+    let hoursSinceDeath = (((milliSinceDeath / 1000) / 60) / 60)
+    let daysSinceDeath = (((milliSinceDeath / 1000) / 60) / 60) / 24;
+    let trueDaysSinceDeath = Math.floor(daysSinceDeath % 365) - leapDays;
+    
+    let years = Math.floor(daysSinceDeath / 365)
+    
+    let monthsInHours = trueDaysSinceDeath * 24;
+    let yearsInHours = (years * 8760) + (leapDays * 24);
+
+    let months
+    let days
+    
+    for (let i = 0; i < monthArray.length; i++) {
+        if (trueDaysSinceDeath <= monthArray[i]) {
+            months = i + 1;
+            days = trueDaysSinceDeath - monthArray[i];
+            break
+        }
+    }
+
+    let hours = Math.floor(hoursSinceDeath - yearsInHours - monthsInHours);
+    let mins = Math.floor(((hoursSinceDeath - yearsInHours - monthsInHours) - hours) * 60);
+    
+    return `Yes! Margaret Thatcher is indeed dead. She has been dead for ${years} years, ${months} month(s), ${days} day(s), ${hours} hour(s), and ${mins} minute(s).`
 }
 
-const sentence = calculator();
-
-export { sentence };
+module.exports.calculator = calculator;
