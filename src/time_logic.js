500465fd784cdb364bb5f68dacebdfd169737b90
conmcdon721 <81780150+conmcdon721@users.noreply.github.com>
2021-09-09 20:23:27 -0400

    Fix: Properly calculate hours and minutes.

--- src/time_logic.js
+++ src/time_logic.js
@@ -22,32 +22,31 @@ const calculator = () => {
     /* ------------------------------------------------------------------------------------------------------------------------------------------------ */
     
     let milliSinceDeath = today - maggieDeathDate - 14400000;
-
     let hoursSinceDeath = ((milliSinceDeath / 1000) / 60) / 60;
     let daysSinceDeath = (((milliSinceDeath / 1000) / 60) / 60) / 24;
     
     const monthArray = [30, 61, 91, 122, 153, 183, 214, 244, 275, 306, 334];
 
     let years = Math.floor(daysSinceDeath / 365);
-    let leftoverDays = daysSinceDeath % 365;
+    let leftoverDays = Math.floor((daysSinceDeath % 365) - leapDays);
+    console.log(leftoverDays)
 
     let months;
     let days;
 
     for (let i = 0; i < monthArray.length; i++) {
         if (leftoverDays < monthArray[i]) {
-            months = i - 1;
-            days = leftoverDays - monthArray[months - 1];
+            months = i;
+            days = Math.floor(leftoverDays - monthArray[i - 1]);
             break;
         };
     };
 
-    let yearsInHours = (years * 8760) + (leapDays * 24);
-    let monthsInHours = leftoverDays * 24;
-    // IF BREAKING, UNCOMMENT THIS - let daysInHours = days * 24; - AND SUBTRACT IT FROM hoursSinceDeath
+    let yearsInHours = years * 8760;
+    let daysInHours = (leftoverDays * 24) + (leapDays * 24);
 
-    let hours = Math.floor(hoursSinceDeath - yearsInHours - monthsInHours);
-    let mins = Math.floor(((hoursSinceDeath - yearsInHours - monthsInHours) - hours) * 60);
+    let hours = Math.floor(hoursSinceDeath - yearsInHours - daysInHours);
+    let mins = Math.floor(((hoursSinceDeath - yearsInHours - daysInHours) - hours) * 60);
 
     return `Yes! Margaret Thatcher is indeed dead. She has been dead for ${years} years, ${months} month(s), ${days} day(s), ${hours} hour(s), and ${mins} minute(s).`;
 };
